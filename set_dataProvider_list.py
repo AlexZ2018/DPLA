55ae35e4bd6b388be34f466861e3c572a8001b02
AlexZ2018 <35750181+AlexZ2018@users.noreply.github.com>
2018-09-24 22:39:52 -0500

    add csv close()

--- set_dataProvider_list.py
+++ set_dataProvider_list.py
@@ -5,60 +5,58 @@ import sys
 import dpla_utils
 import getopt, dpla_config
 import csv
+# argparse
+# plz add comments
 def set_dataProvider_list():
         api_key = dpla_config.API_KEY
-        #set_provider_list.set_provider_list()
-        #csv_reader = csv.reader(open('files/provider_list.csv'))
-        opts, args = getopt.getopt(sys.argv[1:], "hi:o:")
-        output_file, input_file = "",""
+
+        # put this in a seperate function
+        opts, args = getopt.getopt(sys.argv[1:], "hi:e:")
+        folder, input_file = "",""
         for op, value in opts:
-                if op == "-o":
-                        output_file = value
+                if op == "-e":
+                        folder = value                
                 elif op == "-i":
                         input_file = value
         try:
-                csv_reader = csv.reader(open(input_file))
-                header = True
-                count = 0
-                for row in csv_reader:
-                        if header:
-                                header = False
-                                continue
-                        else:
-                                current_provider = row[0]
-                                condition = {'provider.name': current_provider}
-                                condition['facets'] = 'dataProvider'
-                                response = dpla_utils.dpla_fetch_facets_remote(api_key, **condition)
-                                current_dataprovider_list = []
-                                sub_dict = response['dataProvider']
-                                dataprovider_result_list = sub_dict['terms']
-                                count += len(dataprovider_result_list)
-                                #with open(output_file + current_provider + '.csv', 'w')
-                                try:
-                                        with open(output_file + current_provider + '.csv', 'w') as file_open:
-                                                header = ['term', 'count']
-                                                writer = csv.DictWriter(file_open, header)
-                                                writer.writeheader()
-                                                writer.writerows(dataprovider_result_list)
-                                        #header = ['dataProvider.name', 'count']
-                                        #writer.writeheader()
+                with open(input_file) as input_file:
+                        provider_csv_reader = csv.reader(input_file)
+                        header_boolean = True # delete
+                        dataProvider_count = 0 
+                
+                        for provider_information_row in provider_csv_reader: 
+                        
+                                if header_boolean: # i think i can change this by setting a range of rows...
+                                        header_boolean = False
+                                        continue
+                                else:
+                                        current_provider_name = provider_information_row[0] 
 
-                                        with open(output_file + current_provider + '.csv') as file1:
-                                                f_csv = csv.DictReader(file1)
-                                                for index, row in enumerate(f_csv):
-                                                        key = 'provider'
-                                                        value = 'current_provider'
-                                                        row[key] = value
+                                        # comment for query and facet
+                                
+                                        query_terms = {'provider.name': current_provider_name}
+                                        query_terms['facets'] = 'dataProvider'
+                                        dataProvider_query_response = dpla_utils.dpla_fetch_facets_remote(api_key, **query_terms)
+                                        #print(response)
+                                        dataprovider_result_list = dataProvider_query_response['dataProvider']['terms']
+                                        dataProvider_count += len(dataprovider_result_list)
+                                
+                                        try:
+                                                with open(folder + current_provider_name + '.csv', 'w') as dataProvider_file:
+                                                        print(folder + current_provider_name + '.csv');
+                                                        header = ['term', 'count'] # put in begining
+                                                        dataProvider_csv_writer = csv.DictWriter(dataProvider_file, header)
+                                                        dataProvider_csv_writer.writeheader()
+                                                        dataProvider_csv_writer.writerows(dataprovider_result_list)
 
-                                        #print("There are " + str(count) + " dataProviders contribute data for DPLA")
-                                except IOError as f:
-                                        print("please enter a valid output_file")
-                                        sys.exit()
-                print("There are " + str(count) + " dataProviders contribute data for DPLA")
-
-        except IOError as e:
-                print("Please enter a valid input file name. ")
-                #logging.exception(e)
-set_dataProvider_list()
-
-
+                                                dataProvider_file.close();
+                                        
+                                        except IOError as folder_error: 
+                                                print("couldn`t create file in this folder") 
+                                                sys.exit()
+                        print("There are " + str(dataProvider_count) + " dataProviders contribute data for DPLA")
+                        input_file.close();
+        except IOError as input_file_error: 
+                print("couldn`t read the input file ") 
+                
+set_dataProvider_list() # main funciton # close csv
