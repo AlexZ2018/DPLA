f90ef9dd91cfe99502d46669a1e034ea7f153d75
AlexZ2018 <35750181+AlexZ2018@users.noreply.github.com>
2018-10-05 21:48:26 -0500

    standardized variables

--- set_provider_list.py
+++ set_provider_list.py
@@ -5,57 +5,58 @@ This fuction is used for getting all the providers in DPLA with the format {id, 
 import dpla_utils
 import sys, getopt, dpla_config
 import csv
+import argparse
 
-def get_pvd():
+DPLA_PROVIDER_HEADER_NAME = 'provider.name'
+DPLA_PROVIDER_HEADER_COUNT = 'count'
+
+def get_providers():
         api_key = dpla_config.API_KEY
-        condi = {'facets': 'provider'}
-        _dict = dpla_utils.dpla_fetch_facets_remote(api_key,"items",  **condi)
-        result_list = []
-        #travers subarray key: id or name
-        for key, value in _dict.items():
-                _key = key
-                _list = value['terms'] # type: list
-                # treaverse the list of id and name
-                for index in range(len(_list)):
-                        i = index
-                        sub_dict = _list[i]
-                        length = len(result_list)
-                        if i + 1 > length:
-                                unit_dict = {'count': sub_dict['count'], _key: sub_dict['term']}
-                                #print(unit_dict)
-                                result_list.append(unit_dict)
-                        else:
-                                unit_dict = result_list[i]
-                                unit_dict[_key] = sub_dict['term']
-                                result_list[i] = unit_dict
+        query_terms = {'facets': 'provider.name'}
+        facet_response = dpla_utils.dpla_fetch_facets_remote(api_key,  **query_terms)
+        provider_result_list = []
 
-        #print(result_list)
-        #print("The amount of Providers: " + str(len(result_list)))
-        return result_list
+        #travers subarray key: id or name
+        for key, value in facet_response.items():
+                provider_list = value['terms'] # type: list
+                # treaverse the list of id and name
+                for index in range(len(provider_list)):
+                        
+                        sub_facet_response = provider_list[index]
+                        
+                        length = len(provider_result_list)
+                        single_facet_response = {'count': sub_facet_response['count'], key: sub_facet_response['term']}
+                                
+                        provider_result_list.append(single_facet_response)
+                        
+
+        return provider_result_list
 
 def set_provider_list():
 
-        opts, args = getopt.getopt(sys.argv[1:], "ho:")
-        output_file = ""
-        for op, value in opts:
-                if op == "-o":
-                        output_file = value
-                else:
-                        print("please enter a valid output file")
-                        sys.exit()
+        # Read output file from command line
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-o', action='append', dest = 'output_file_list')
+        results = parser.parse_args()
 
-        #provider_list = get_pvd()
+        output_file = results.output_file_list[0]
+        
         try:
-                provider_list = get_pvd()
-                with open(output_file, 'w') as file_open:
-                        file_header = ['provider.name', 'provider.@id', 'count']
-                        writer = csv.DictWriter(file_open, file_header)
-                        writer.writeheader()
-                        writer.writerows(provider_list)
+                provider_list = get_providers()
+                
+                with open(output_file, 'w') as provider_file_open:
+                        file_header = [DPLA_PROVIDER_HEADER_NAME, DPLA_PROVIDER_HEADER_COUNT]
+                        provider_writer = csv.DictWriter(provider_file_open, file_header)
+                        provider_writer.writeheader()
+                        provider_writer.writerows(provider_list)
 
                         print("List of providers has been successfully written to files as provider_list.csv")
                         print("The amount of Providers: " + str(len(provider_list)))
         except IOError as e:
-                print("please enter a valid output file")
+                print("cannot open output file")
 
-set_provider_list()
+
+def main():
+        set_provider_list()
+
+main()
